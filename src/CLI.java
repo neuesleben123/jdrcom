4de230f50557b08e7e1bc16a4a72f81d1781567a
douniwan5788 <douniwan5788@gmail.com>
2011-11-14 12:29:23 +0000

    第二步登陆还有问题

--- src/CLI.java
+++ src/CLI.java
@@ -18,11 +18,13 @@ import org.apache.commons.cli.PosixParser;
 
 class CLI implements MessageListener {
 
-	NetworkInterface nif;
+	LoginInfo logif=new LoginInfo();
+
+	/*NetworkInterface nif;
 	byte[] src_mac;
 	byte[] dst_mac;
 	String UserName;
-	String PassWord;
+	String PassWord;*/
 	char type = 'i';
 	Options options = new Options();
 
@@ -31,6 +33,7 @@ class CLI implements MessageListener {
 	}
 
 	private void printHelp(Options options) {
+		System.out.println("\n因学校无IPV6环境，无法测试，本软件暂不支持IPV6！如果你想帮助作者完善请访问 https://code.google.com/p/jdrcom/\n");
 		HelpFormatter formatter = new HelpFormatter();
 		formatter.printHelp("drcom", options);
 	}
@@ -50,10 +53,10 @@ class CLI implements MessageListener {
 		options.addOption("p", "password", true, "密码,默认123456");
 		options.addOption("svr", "serverip", true, "外网登陆服务器ip 默认自动搜索");// 为10.5.2.3
 		options.addOption("ds", "dhcpscript", true,
-				"内网拨号成功后自定义的自动获取IP脚本命令,Windows下默认为\"ipconfig /renew *\",Linux默认为\"dhcpc *\" ");
+				"内网拨号成功后自定义的自动获取IP脚本命令\nWindows下默认为\"ipconfig /renew *\"\nLinux默认为\"dhcpc *(待考虑)\" ");
 		options.addOption("s", "srcmac", true, "指定源MAC地址，比如你用别人的账号上网");
 		options.addOption("d", "dstmac", true,
-				"指定目的MAC地址，默认为01-80-C2-00-00-03，一般不用指定");
+				"指定目的MAC地址,默认为01-80-C2-00-00-03,一般不用指定");
 		options.addOption("h", "help", false, "显示此帮助");
 		// String[] args = new String[]{ "--block-size=10" };
 
@@ -88,8 +91,8 @@ class CLI implements MessageListener {
 			if (line.hasOption('i')) {
 				for (NetworkInterface n : JpcapCaptor.getDeviceList())
 					if (n.name.equals(line.getOptionValue('i')))
-						nif = n;
-				if (nif == null) {
+						logif.nif = n;
+				if (logif.nif == null) {
 					System.out.println("网卡不存在!请使用-l显示网卡列表");
 					return false;
 				}
@@ -111,11 +114,11 @@ class CLI implements MessageListener {
 					System.out.println("不合法的源MAC地址！");
 					return false;
 				}
-				src_mac = new BigInteger(strmac, 16).toByteArray();
-				src_mac = Arrays.copyOfRange(src_mac, src_mac.length - 6,
-						src_mac.length);
+				logif.src_mac = new BigInteger(strmac, 16).toByteArray();
+				logif.src_mac = Arrays.copyOfRange(logif.src_mac, logif.src_mac.length - 6,
+						logif.src_mac.length);
 			} else {
-				src_mac = nif.mac_address;
+				logif.src_mac = logif.nif.mac_address;
 			}
 
 			if (line.hasOption('d')) {
@@ -125,19 +128,19 @@ class CLI implements MessageListener {
 					System.out.println("不合法的目的MAC地址！");
 					return false;
 				}
-				dst_mac = new BigInteger(dstmac, 16).toByteArray();
+				logif.dst_mac = new BigInteger(dstmac, 16).toByteArray();
 			} else {
-				dst_mac = nif.mac_address;
+				logif.dst_mac = logif.nif.mac_address;
 			}
 
 			if (line.hasOption('u'))
-				UserName = line.getOptionValue('u');
+				logif.UserName = line.getOptionValue('u');
 			else {
 				System.out.println("必须指定用户名");
 				return false;
 			}
 
-			PassWord = line.getOptionValue('p', "123456");
+			logif.PassWord = line.getOptionValue('p', "123456");
 
 		} catch (ParseException exp) {
 			System.out.println("错误:" + exp.getMessage());
@@ -148,8 +151,7 @@ class CLI implements MessageListener {
 
 		switch (type) {
 		case 'i':
-			innerNetwork in = new innerNetwork(nif, src_mac, dst_mac, UserName,
-					PassWord);
+			innerNetwork in = new innerNetwork(logif);
 			in.addMessageListener(this);
 			in.login();
 			break;
@@ -157,8 +159,7 @@ class CLI implements MessageListener {
 			InetAddress address;
 			try {
 				address = InetAddress.getByAddress(new byte[] { 10, 5, 2, 3 });
-				outerNetwork out = new outerNetwork(UserName, PassWord,
-						address, -1);
+				outerNetwork out = new outerNetwork(logif,-1);
 				out.addMessageListener(this);
 				out.login();
 			} catch (UnknownHostException e) {
