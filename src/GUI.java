f8ccc29c5df75f159c2307d2313eb2a0550d8189
sdlixiangxiang <sdlixiangxiang@gmail.com>
2011-12-15 03:38:03 +0000

    实验改进GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,116 +1,126 @@
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+ 
+import java.awt.*;
+//import java.awt.Event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
-class GUI {
-	private JFrame jframe;
-	private JLabel jlabel, jlabel1;
-	private GridBagLayout gridbag;
-	private GridBagConstraints constraints;
-	private JTextField jtfield1;
-	private JPasswordField jpfield1;
-	private JButton jbutton1, jbutton2, jbutton3;
 
-	public GUI() {
-		jframe = new JFrame();
-		jlabel = new JLabel();
-		jlabel1 = new JLabel();
-		jtfield1 = new JTextField();
-		jpfield1 = new JPasswordField();
-		gridbag = new GridBagLayout();
-		jbutton1 = new JButton();
-		jbutton2 = new JButton();
-		jbutton3 = new JButton();
+ public class GUI  implements ActionListener , WindowListener
+ {
+	
+	 TextField textFieldusername ;
+	 TextField textFieldpwd ;
 
-		init();
+	   
+		
+	 public void paint()
+	
+	  {
+		
+		 
+		 Frame f1 =new Frame();
+		 f1.addWindowListener( new GUI());  // 关闭窗口
+		 // Frame f2 =new Frame("lixiang");
+		  
+		   f1.setSize(100,200);
+		  //f1.setSize(100,200);
+		 //f1.setLayout(new GridLayout(5,5));
+		 
+		  
+		  f1.setBackground(Color.YELLOW);
+		   Button b1 =new Button("one");
+		   Button b2 =new Button("two");
+		   Button b3 =new Button("tree");
+		   Button b4 =new Button("four");
+		   Button b5 =new Button("five");
+		   
+		   BorderLayout e= new  BorderLayout(0,0);
+		   
+		   e.setHgap(5);
+		   e.setVgap(5);
+		   
+		   f1.setLayout(e);
+		   f1.setLayout(new FlowLayout());
+		   
+		   FlowLayout f=new FlowLayout(FlowLayout.RIGHT,10,20);
+		   
+		   f.setAlignment(FlowLayout.LEFT);
+		   
+		   
+		   f1.add(b1,BorderLayout.EAST);
+		   f1.add(b2,BorderLayout.NORTH);
+		   f1.add(b3,BorderLayout.SOUTH);
+		   f1.add(b4,BorderLayout.WEST);
+		   f1.add(b5,BorderLayout.CENTER);
+		   
+		   //CardLayout card;
+		   
+		   b1.addActionListener(this);
+		   b1.setActionCommand("one");
+		   
+		   
+		   b2.addActionListener(this);
+		   b2.setActionCommand("two"); 
+		   
+		  // b3.addActionListener(this);
+		  
+		   
+		   Label labelusername=new Label("username",Label.LEFT);
+		   f1.add(labelusername);
+		   Label labelpwd=new Label("password",Label.LEFT);
+		  
+		   f1.add(labelusername);
+		   f1.add(labelpwd);
+		 textFieldusername=new TextField(15);
+		   textFieldpwd=new TextField(15);
+		   
+		   textFieldpwd.setEchoChar('*');
+		  // int a[][]={{0,0,1,1}};
+		  f1.add(textFieldusername);
+		  f1.add(textFieldpwd);
+		  
+		  
+		   
+		   //showStatus(getuser);
+		   
+		  f1.pack(); 
+		  f1.setVisible(true);
+		  //f2.setVisible(true);
+		 
+		
+		
+	  
+	 }
+	 public void windowClosing(WindowEvent e)
+		{
+		   System.exit(0);	
+		}
+	 public void  windowDeactivated(WindowEvent e){}
+	 public void windowActivated(WindowEvent e){}
+     public void windowOpened(WindowEvent e){}
+	 public void windowIconified (WindowEvent e){}
+	 public void  windowClosed(WindowEvent e){}
+	 public void  windowDeiconified(WindowEvent e){}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+		 String  getuser1,getpwd2;
+		 String cmd =e.getActionCommand();
+		 if(cmd.equals("one"))
+		 {
+		   getuser1=textFieldusername.getText();
+		   getpwd2=textFieldpwd.getText();	
+		   
+		   System.out.print(getuser1);
+		 }
+		 else 
+		 {}
 	}
 
-	private void init() {
-		jframe.setTitle("全屏幕测试");
-		jlabel.setText("用户名：");
-		jlabel1.setText("密    码：");
-		jbutton1.setText("登    录");
-		jbutton2.setText("退    出");
-		jbutton3.setText("更改密码");
-		// jframe.setUndecorated(true);
-//		jframe.getGraphicsConfiguration().getDevice().setFullScreenWindow(
-//				jframe);
-		jframe.setLayout(gridbag);
-
-		// 用户名文本框显示
-		constraints = getGridBagConstraints(0, 0, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 0, 0);
-
-		gridbag.setConstraints(jlabel, constraints);
-		jframe.add(jlabel);
-
-		// 用户名输入框显示
-		constraints = getGridBagConstraints(1, 0, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 100, 0);
-
-		gridbag.setConstraints(jtfield1, constraints);
-		jframe.add(jtfield1);
-
-		// 密码文本框显示
-		constraints = getGridBagConstraints(0, 1, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 0, 0);
-		gridbag.setConstraints(jlabel1, constraints);
-		jframe.add(jlabel1);
-
-		// 密码输入框显示
-		constraints = getGridBagConstraints(1, 1, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 100, 0);
-
-		gridbag.setConstraints(jpfield1, constraints);
-		jframe.add(jpfield1);
-
-		// 更改密码按钮显示
-		constraints = getGridBagConstraints(0, 2, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 0, 0);
-
-		gridbag.setConstraints(jbutton3, constraints);
-		jframe.add(jbutton3);
-
-		// 登录按钮显示
-		constraints = getGridBagConstraints(1, 2, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 0, 0);
-
-		gridbag.setConstraints(jbutton1, constraints);
-		jframe.add(jbutton1);
-
-		// 退出按钮显示
-		constraints = getGridBagConstraints(2, 2, 1, 1, 0, 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
-						10, 0, 10, 0), 0, 0);
-
-		gridbag.setConstraints(jbutton2, constraints);
-		jframe.add(jbutton2);
-
-	}
-
-	private static GridBagConstraints getGridBagConstraints(int gridx,
-			int gridy, int gridwidth, int gridheight, double weightx,
-			double weighty, int anchor, int fill, Insets insets, int ipadx,
-			int ipady) {
-
-		return new GridBagConstraints(gridx, gridy, gridwidth, gridheight,
-				weightx, weighty, anchor, fill, insets, ipadx, ipady);
-	}
-
-	public void showMe() {
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setVisible(true);
-	}
 }
